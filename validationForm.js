7387432e3a9dbfa68dd8bbd85dfc3c9e00f52347
Sameermurtaza12 <107950748+Sameermurtaza12@users.noreply.github.com>
2022-07-15 05:44:14 -0400

    Add files via upload

--- validationForm.js
+++ validationForm.js
@@ -14,12 +14,15 @@ try{
     if(!dob) throw "Empty dob field"
     if(!interest) throw "Empty interest field"
     if(!Country) throw "Empty country field"
-    if(!individual) throw "Empty individual field"
-    if(!organization) throw "Empty organization field"    
+    if(!(individual || organization)) throw "Empty individual or organization field"
+    //if(!organization) throw "Empty organization field"    
     if(!aboutinfo) throw "Empty aboutinfo field"   
+    var qs="summary.html?fname="+fname+"&email="+email+"&dob="+dob+"&interest="+interest+"&Country="+Country+"&individual="+individual+"&organization="+organization+"&aboutinfo="+aboutinfo
+    window.open(qs)
 }
 catch (err){
     window.alert(err)
 }
+
 
 }
